148d92b386c7350ed5c471fa952fb76dd3d22991
Zach Lendon <zach.lendon@gmail.com>
2012-09-12 02:14:51 -0500

    added timer

--- Kobold2D-2.0.3/HoopyDrop/Projectfiles/TextOverlayLayer.mm
+++ Kobold2D-2.0.3/HoopyDrop/Projectfiles/TextOverlayLayer.mm
@@ -8,7 +8,10 @@
 
 #import "TextOverlayLayer.h"
 
-@implementation TextOverlayLayer
+@implementation TextOverlayLayer {
+	int countTime;
+	double lastUpdateTime;
+}
 
 -(id) init
 {
@@ -32,18 +35,45 @@
         
         // Default font size will be 28 points.
 		[CCMenuItemFont setFontSize:28];
-//        
-//		CCMenuItem *resume = [CCMenuItemFont itemWithString:@"Resume Game" target:self selector:@selector(handleUnpause)];
-//        CCMenuItem *endGame = [CCMenuItemFont itemWithString:@"End Level" target:self selector:@selector(handleEndLevel)];
-//        
-//        CCMenu *menu = [CCMenu menuWithItems:resume, endGame, nil];
 		
-//		[menu alignItemsHorizontallyWithPadding:20];
-//		[menu setPosition:ccp( size.width/2, size.height/2 - 50)];
-//		[self addChild:menu];
+		
+		//		//timer
+		        CCLabelBMFont *timerText = [CCLabelBMFont labelWithString:@"60" fntFile:@"hd-font.fnt"];
+				[timerText setTag:1];
+		        timerText.position =  ccp(size.width - (.1*size.width), size.height - (.1*size.height));
+		
+		 
+		countTime = 60;
+		lastUpdateTime = CACurrentMediaTime();
+		
+				[self addChild: timerText];
+		        [self schedule: @selector(tick:) interval:1.0];
+		//
+		//		CCMenuItem *resume = [CCMenuItemFont itemWithString:@"Resume Game" target:self selector:@selector(handleUnpause)];
+		//        CCMenuItem *endGame = [CCMenuItemFont itemWithString:@"End Level" target:self selector:@selector(handleEndLevel)];
+		//
+		//        CCMenu *menu = [CCMenu menuWithItems:resume, endGame, nil];
+		
+		//		[menu alignItemsHorizontallyWithPadding:20];
+		//		[menu setPosition:ccp( size.width/2, size.height/2 - 50)];
+		//		[self addChild:menu];
         
     }
     return self;
+}
+
+-(void) tick: (ccTime) dt
+{	
+	CCLabelBMFont *timerText = (CCLabelBMFont *)[self getChildByTag:1];
+	
+	if (CACurrentMediaTime() - lastUpdateTime >= 1) {
+		countTime--;
+		[timerText setString:[NSString stringWithFormat:@"%i", countTime]];
+	}
+	
+	if (countTime == 0)
+        [self unschedule: @selector(tick:)];
+
 }
 
 @end
