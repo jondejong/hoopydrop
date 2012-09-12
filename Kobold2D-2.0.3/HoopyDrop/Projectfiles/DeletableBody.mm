9d15a3a8d3eaab5ef17315dbe8af3b2aaf67278c
Jon DeJong <jon.dejong@gmail.com>
2012-09-11 23:27:21 -0600

    Adding Font

---
+++ Kobold2D-2.0.3/HoopyDrop/Projectfiles/DeletableBody.mm
@@ -0,0 +1,24 @@
+//
+//  DeletableBody.m
+//  HoopyDrop
+//
+//  Created by Jon DeJong on 9/11/12.
+//
+//
+
+#import "HoopyDrop.h"
+#import "Box2d.h"
+
+@implementation DeletableBody {
+    @private
+    b2Body* _body;
+}
+
+-(b2Body*) body {
+    body
+}
+-(void) setBody: (b2Body*) body;
+
+
+
+@end
