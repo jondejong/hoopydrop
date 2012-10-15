072b90e3cb6009ea1163177ec87d7ec472e287a0
Jon DeJong <jon.dejong@gmail.com>
2012-10-15 17:30:58 -0500

    continuing to tweak leader board screen.

--- Kobold2D-2.0.3/HoopyDrop/LeaderBoardLayer.mm
+++ Kobold2D-2.0.3/HoopyDrop/LeaderBoardLayer.mm
@@ -16,9 +16,9 @@
     if (self) {
         CGSize size = [[CCDirector sharedDirector] winSize];
         
-        CCSprite* buttonBack = [CCSprite spriteWithFile:@"button-back.png"];
-        buttonBack.position = ccp(size.width/2, size.height - 35);
-        [self addChild: buttonBack z:BACKGROUND_Z+1];
+//        CCSprite* buttonBack = [CCSprite spriteWithFile:@"button-back.png"];
+//        buttonBack.position = ccp(size.width/2, size.height - 35);
+//        [self addChild: buttonBack z:BACKGROUND_Z+1];
         
         CCLabelTTF *screenLabel = [CCLabelTTF labelWithString:@"High Scores" fontName:@"Marker Felt" fontSize:24];
         
@@ -36,25 +36,26 @@
         CCSpriteBatchNode* backBar = [CCSpriteBatchNode batchNodeWithFile:@"score-back.png" capacity:10];
         [self addChild:backBar z:OBJECTS_Z];
         
-        float nextY = size.height - 75;
+        float nextY = size.height/2 + 140;
         
         CCSprite* barSprite = [CCSprite spriteWithTexture:[sepBar texture]];
-        barSprite.position = CGPointMake(0, nextY);
-        barSprite.anchorPoint = CGPointMake(0, 0);
+        barSprite.position = CGPointMake(size.width/2, nextY);
+        
+        barSprite.anchorPoint = CGPointMake(.5, 0);
         [sepBar addChild:barSprite z:OBJECTS_Z];
-        nextY -= 35;
+        nextY -= 30;
         
         for(int i=0; i<10; i++) {
             CCSprite* barSprite = [CCSprite spriteWithTexture:[sepBar texture]];
             CCSprite* backBarSprite = [CCSprite spriteWithTexture:[backBar  texture]];
-            backBarSprite.position = CGPointMake(0, nextY);
-            backBarSprite.anchorPoint = CGPointMake(0, 0);
+            backBarSprite.position = CGPointMake(size.width/2, nextY);
+            backBarSprite.anchorPoint = CGPointMake(.5, 0);
             nextY -= 5;
-            barSprite.position = CGPointMake(0, nextY);
-            barSprite.anchorPoint = CGPointMake(0, 0);
+            barSprite.position = CGPointMake(size.width/2, nextY);
+            barSprite.anchorPoint = CGPointMake(.5, 0);
             [sepBar addChild:barSprite z:OBJECTS_Z];
             [backBar addChild:backBarSprite z:OBJECTS_Z];
-            nextY -= 35;
+            nextY -= 30;
         }
         
         resetMenu.position = ccp(12, size.height - 35);
@@ -65,16 +66,26 @@
         [self addChild:screenLabel z:OBJECTS_Z];
         
         int count = 1;
-        nextY = size.height - 95;
+        nextY = size.height/2 + 120;
         for(NSNumber* score in [[GameManager sharedInstance] highScores]) {
-            CCLabelTTF *scoreLabel = [CCLabelTTF labelWithString:[NSString stringWithFormat:@"%i", [score integerValue]] fontName:@"Marker Felt" fontSize:24];
-            CCLabelTTF *number = [CCLabelTTF labelWithString:[NSString stringWithFormat:@"%i:", count] fontName:@"Marker Felt" fontSize:24];
-            scoreLabel.position = ccp(size.width/2, nextY);
-            number.position = ccp(20, nextY);
-            nextY-=40;
+            NSString* scoreString = [NSString stringWithFormat:@"%i", [score integerValue]];
+            CCLabelBMFont *scoreLabel = [CCLabelBMFont labelWithString:scoreString fntFile:@"hdfont-full-small.fnt"];
+            
+            [scoreLabel setAlignment:kCCTextAlignmentRight];
+            scoreLabel.position = ccp(size.width/2 + 125, nextY);
+            scoreLabel.anchorPoint = ccp(1, 0);
+            
+            NSString* numberString = [NSString stringWithFormat:@"%i:", count];
+            CCLabelBMFont *numberLabel = [CCLabelBMFont labelWithString:numberString fntFile:@"hdfont-full-small.fnt"];
+
+//            [numberLabel setAlignment:kCCTextAlignmentRight];
+            numberLabel.position = ccp(size.width/1.8 - 125, nextY);
+            numberLabel.anchorPoint = ccp(0, 0);
+            
+            nextY-=35;
             count++;
             [self addChild:scoreLabel z:OVERLAY_TEXT_Z];
-            [self addChild:number z:OVERLAY_TEXT_Z];
+            [self addChild:numberLabel z:OVERLAY_TEXT_Z];
         }
         
     }
