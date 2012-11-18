815bd1bb63fd6238071125cbe0b9b2f77c408da0
Jon DeJong <jon.dejong@gmail.com>
2012-11-18 13:29:47 -0600

    Fixing bugs with overlapping time windows for power ups.

--- Kobold2D-2.0.3/HoopyDrop/Projectfiles/HDTimer.mm
+++ Kobold2D-2.0.3/HoopyDrop/Projectfiles/HDTimer.mm
@@ -55,7 +55,7 @@
         _cherryTargetRemoved = NO;
         _boltTargetAdded = NO;
         _boltTargetRemoved = NO;
-   
+        
 #if DRAW_TNT
         _bombAddTime = (arc4random() % (GOODIE_BEGIN_TIME - GOODIE_END_TIME)) + GOODIE_END_TIME;
         _bombExpireTime = _bombAddTime - GOODIE_EXPIRE_SECONDS;
@@ -72,35 +72,56 @@
         _boltExpireTime = _bombExpireTime;
         _cherryExpireTime = _bombExpireTime;
         
-
+        while(![self validateTimeWindows]) {
 #if DRAW_EXTRA_TIME
-        while( (_extraSecondsAddTime <= _bombAddTime && _extraSecondsAddTime >=_bombExpireTime) ||
-              (_extraSecondsAddTime <= _cherryAddTime && _extraSecondsAddTime >= _cherryExpireTime) ||
-              (_extraSecondsAddTime <= _boltAddTime && _extraSecondsAddTime >= _boltExpireTime)){
             _extraSecondsAddTime = (arc4random() % (GOODIE_BEGIN_TIME - GOODIE_END_TIME)) + GOODIE_END_TIME;
-        }
-        _extraSecondsExpireTime = _extraSecondsAddTime - GOODIE_EXPIRE_SECONDS;
-        
+            _extraSecondsExpireTime = _extraSecondsAddTime - GOODIE_EXPIRE_SECONDS;
 #endif
-        
+            
 #if DRAW_BOLT
-        while((_boltAddTime <= _bombAddTime && _boltAddTime >= _bombExpireTime) ||
-              (_boltAddTime <= _extraSecondsAddTime && _boltAddTime >= _extraSecondsExpireTime) ||
-              (_boltAddTime <= _cherryAddTime && _boltAddTime >= _cherryExpireTime)){
             _boltAddTime = (arc4random() % (GOODIE_BEGIN_TIME - GOODIE_END_TIME)) + GOODIE_END_TIME;
-        }
-        _boltExpireTime = _boltAddTime - GOODIE_EXPIRE_SECONDS;
+            _boltExpireTime = _boltAddTime - GOODIE_EXPIRE_SECONDS;
 #endif
 #if DRAW_CHERRY
-        while((_cherryAddTime <= _bombAddTime && _cherryAddTime >= _bombExpireTime) ||
-              (_cherryAddTime <= _extraSecondsAddTime && _cherryAddTime >= _extraSecondsExpireTime) ||
-              (_cherryAddTime <= _boltAddTime && _cherryAddTime >= _boltExpireTime)){
             _cherryAddTime = (arc4random() % (CHERRY_BEGIN_TIME - CHERRY_END_TIME)) + CHERRY_END_TIME;
-        }
-        _cherryExpireTime = _cherryAddTime - GOODIE_EXPIRE_SECONDS;
+            _cherryExpireTime = _cherryAddTime - GOODIE_EXPIRE_SECONDS;
 #endif
+        }
     }
     return self;
+}
+
+-(bool) validateTimeWindows
+{
+    // Validate Extra Seconds
+    if( (_extraSecondsAddTime <= _bombAddTime && _extraSecondsAddTime >=_bombExpireTime) ||
+        (_extraSecondsAddTime <= _cherryAddTime && _extraSecondsAddTime >= _cherryExpireTime) ||
+        (_extraSecondsAddTime <= _boltAddTime && _extraSecondsAddTime >= _boltExpireTime)) {
+        return NO;
+    }
+    
+    // Validate Bold
+    if((_boltAddTime <= _bombAddTime && _boltAddTime >= _bombExpireTime) ||
+     (_boltAddTime <= _extraSecondsAddTime && _boltAddTime >= _extraSecondsExpireTime) ||
+       (_boltAddTime <= _cherryAddTime && _boltAddTime >= _cherryExpireTime)) {
+        return NO;
+    }
+    
+    // Validate Cherry
+    if((_cherryAddTime <= _bombAddTime && _cherryAddTime >= _bombExpireTime) ||
+          (_cherryAddTime <= _extraSecondsAddTime && _cherryAddTime >= _extraSecondsExpireTime) ||
+          (_cherryAddTime <= _boltAddTime && _cherryAddTime >= _boltExpireTime)){
+        return NO;
+    }
+    
+    // Validate Bomb
+    if((_bombAddTime <= _extraSecondsAddTime && _bombAddTime >= _extraSecondsExpireTime) ||
+       (_bombAddTime <= _boltAddTime && _bombAddTime >= _boltExpireTime) ||
+       (_bombAddTime <= _cherryAddTime && _bombAddTime >= _cherryExpireTime)) {
+        return NO;
+    }
+    
+    return YES;
 }
 
 -(void)start {
